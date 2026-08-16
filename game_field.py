92999eb9d83ac3e60c8c1e1f9d22e76fa449ff3a
jbt <hilaterem2008@gmail.com>
2026-08-16 12:37:07 +0300

    t

--- game_field.py
+++ game_field.py
@@ -1,16 +1,16 @@
-from consts import BOARD_HEIGHT , BOARD_WIDTH
+from consts import AMOUNT_OF_COLS , AMOUNT_OF_ROWS
 import random
 
 def create_start_board() :
-    board = [[[] for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
+    board = [[[] for _ in range(AMOUNT_OF_COLS)] for _ in range(AMOUNT_OF_ROWS)]
 
 
 def get_possible_mine_positions() :
     list_options_mine_row = []
     list_options_mine_col = []
-    for r in range(BOARD_HEIGHT):
+    for r in range(AMOUNT_OF_ROWS):
         list_options_mine_row.append(r)
-    for c in range(BOARD_WIDTH):
+    for c in range(AMOUNT_OF_COLS):
         list_options_mine_col.append(c)
 
 
@@ -23,6 +23,8 @@ def get_random_mine_position(list_options) :     #Call this function twice - onc
             to_append = random.choice(list_options)
         mine_position_options.append(to_append)
     return mine_position_options
+
+
 
 
 
