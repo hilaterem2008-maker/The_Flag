ee58bf2f587195595968428e6e681bea2e386c62
jbt <aya786321@gmail.com>
2026-08-16 16:27:50 +0300

    added changes

---
+++ screen.py
@@ -0,0 +1,38 @@
+import pygame
+import consts
+import random
+import soldier
+
+screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT ))
+
+
+def draw_grass(grass_img):
+    grassIMG = pygame.image.load(grass_img)
+    grass = pygame.transform.scale(grassIMG, (consts.SIDE_LENGTH, consts.SIDE_LENGTH))
+    for i in range(20):
+        x_location = random.randrange(0, consts.WINDOW_HEIGHT)
+        y_location = random.randrange(0, consts.WINDOW_WIDTH)
+        screen.blit(grass, (x_location, y_location))
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    screen.blit(text_img, location)
+
+def create_main_screen():
+    screen.fill(consts.BOARD_COLOR)
+    draw_grass("grass.png")
+    draw_message("Welcome to the flag game! \n have fun :)" , 11, "black", (10, 20) )
+    soldier.create_soldier("soldier.png")
+    pygame.display.flip()
+
+board = create_main_screen()
+while True:
+    pygame.quit()
+
+
+
+
+
+
+
---
+++ soldier.py
@@ -0,0 +1,39 @@
+import consts
+import pygame
+
+
+def create_soldier(soldier_img):
+    soldierIMG = pygame.image.load(soldier_img)
+    soldier = pygame.transform.scale(soldierIMG, (consts.SIDE_LENGTH, consts.SIDE_LENGTH))
+
+def soldier_body_index(soldier_location):
+    soldier_y = soldier_location[1]
+    soldier_x = soldier_location[0]
+    body_indexes = []
+    for i in range(3):
+        soldier_y += consts.SIDE_LENGTH
+        board_row = soldier_y / consts.SIDE_LENGTH
+        for j in range(2):
+            soldier_x += consts.SIDE_LENGTH
+            board_col = soldier_x / consts.SIDE_LENGTH
+            body_indexes.append(board_row, board_col)
+    return body_indexes
+
+def soldier_legs_location(soldier_location):
+    soldier_y = soldier_location[1]
+    soldier_x = soldier_location[0]
+    leg_indexes = []
+    soldier_y += 3 * consts.SIDE_LENGTH
+    board_row = soldier_y/ consts.SIDE_LENGTH
+    for i in range(2):
+        soldier_x += consts.SIDE_LENGTH
+        board_col = soldier_x / consts.SIDE_LENGTH
+        leg_indexes.append(board_row, board_col)
+    return leg_indexes
+
+
+
+
+
+
+
